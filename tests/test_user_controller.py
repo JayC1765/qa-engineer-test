9851a997d27802c4437b421ad50363454fc8a5a6
Jason Chan <jason.chann91@gmail.com>
2023-07-26 08:46:01 -0400

    created test for validate_user with existing username

--- tests/test_user_controller.py
+++ tests/test_user_controller.py
@@ -23,8 +23,16 @@ def sample_user_data():
     return UserSchemaBase(username="test_user")
 
 
+@pytest.fixture
+def clear_tables(db_session):
+    db_session.query(User).delete()
+    db_session.commit()
+
+    print("tables have been cleared")
+
+
 # Test the UserController.validate_user function for valid input
-def test_validate_user_valid_input(db_session, sample_user_data):
+def test_validate_user_valid_input(clear_tables, db_session, sample_user_data):
     user_controller = UserController()
     response = user_controller.validate_user(db_session, sample_user_data)
     assert response is None
@@ -38,7 +46,7 @@ integers are being coerced to strings and can still be added to database.
 
 # Test the validate_user function for invalid username (not a string)
 @pytest.mark.skip()
-def test_validate_user_invalid_input(db_session):
+def test_validate_user_invalid_input(clear_tables, db_session):
     user_controller = UserController()
     invalid_data = UserSchemaBase(username=123)
     response = user_controller.validate_user(db_session, invalid_data)
@@ -47,10 +55,27 @@ def test_validate_user_invalid_input(db_session):
 
 
 # Test the validate_user function for username less than 3 characters
-def test_validate_user_short_username(db_session):
+def test_validate_user_short_username(clear_tables, db_session):
     user_controller = UserController()
     invalid_data = UserSchemaBase(username="ab")
     response = user_controller.validate_user(db_session, invalid_data)
     assert response is not None
     assert response.status_code == 422
     assert response.detail == "Username must be at least 3 characters"
+
+
+# Test the validate_user function for existing username
+def test_validate_user_existing_username(clear_tables, db_session, sample_user_data):
+    user_controller = UserController()
+
+    # Create a user in the database and return save the username to a variable
+    user_added = user_controller.create_user(db_session, sample_user_data)
+    assert isinstance(user_added, User)
+    username = user_added.username
+
+    # Attempt to add a user with the same username
+    duplicate_username = UserSchemaBase(username=f"{username}")
+    response = user_controller.validate_user(db_session, duplicate_username)
+    assert response is not None
+    assert response.status_code == 409
+    assert response.detail == "Username already exists"
