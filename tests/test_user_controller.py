b579af9e6b589858b9c6b29959ea004022a8c7aa
Jason Chan <jchan@Jasons-MBP.local>
2023-07-25 22:53:37 -0400

    test for validate_user with integers

--- tests/test_user_controller.py
+++ tests/test_user_controller.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pytest
+from fastapi import HTTPException
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
@@ -27,3 +28,19 @@ def test_validate_user_valid_input(db_session, sample_user_data):
     user_controller = UserController()
     response = user_controller.validate_user(db_session, sample_user_data)
     assert response is None
+
+
+"""
+The function below is currently not behaving as expected - values that are
+integers are being coerced to strings and can still be added to database.
+"""
+
+
+# Test the validate_user function for invalid username (not a string)
+@pytest.mark.skip()
+def test_validate_user_invalid_input(db_session):
+    user_controller = UserController()
+    invalid_data = UserSchemaBase(username=123)
+    response = user_controller.validate_user(db_session, invalid_data)
+    assert response.status_code == 422
+    assert response.detail == "Username must be a string"
