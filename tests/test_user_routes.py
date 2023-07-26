bebc9a7cb0038026a84001bffd33085c37f967db
Jason Chan <jason.chann91@gmail.com>
2023-07-26 09:31:29 -0400

    create test for create_user endpoint

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -8,6 +8,9 @@ sys.path.append(parent_dir)
 from main import app
 from fastapi.testclient import TestClient
 from models.users import User, UserSchemaBase
+from database.db import get_db
+
+client = TestClient(app)
 
 
 # Function that returns an instance of a database session
@@ -21,3 +24,21 @@ def db_session():
 def clear_tables(db_session):
     db_session.query(User).delete()
     db_session.commit()
+
+
+# Function that returns a sample of the user_data
+@pytest.fixture
+def sample_user_data():
+    return {"username": "test_user"}
+
+
+# Validate the endpoint for creating a user
+def test_create_user_endpoint(clear_tables, sample_user_data):
+    # Send a POST request to the /users endpoint passing in the sample user data
+    response = client.post("/users", json=sample_user_data)
+    assert response.status_code == 200
+
+    # Validate that the username of the returned user equals the username in the payload
+    data = response.json()
+    assert "username" in data
+    assert data["username"] == sample_user_data["username"]
