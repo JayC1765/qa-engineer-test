02e84c7c19ba92d8c5f713eb708c4c701f7e4250
Jason Chan <jchan@Jasons-MBP.local>
2023-07-25 22:44:21 -0400

    set up test directory structure

---
+++ tests/test_loan_routes.py
@@ -0,0 +1,10 @@
+
+
+# # Test the UserController.validate_user function for valid input
+# def test_validate_user_valid_input(clear_tables, db_session, sample_user_data):
+#     user_controller = UserController()
+#     print('sample user data: ', type(sample_user_data))
+#     print('sample user data: ', sample_user_data.username)
+#     response = user_controller.validate_user(db_session, sample_user_data)
+#     assert response is None
+
