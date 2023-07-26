a1ca259900bf4d99a7df2d35c39e1d07f3a6f61c
Jason Chan <jason.chann91@gmail.com>
2023-07-26 09:51:45 -0400

    created test for get_all_users endpoint

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -42,3 +42,28 @@ def test_create_user_endpoint(clear_tables, sample_user_data):
     data = response.json()
     assert "username" in data
     assert data["username"] == sample_user_data["username"]
+
+
+# Validate the endpoint for getting all users
+def test_get_all_users_endpoint(clear_tables, db_session):
+    # Add 5 users to the database
+    add_users_to_db(db_session, 5)
+
+    # Send a GET request to the /users endpoint
+    response = client.get("/users")
+    assert response.status_code == 200
+
+    # Test that the number of users created equals all users in the database
+    data = response.json()
+    assert len(data) == 5
+
+
+# Helper function to add a n # of unique users to the database
+def add_users_to_db(db, num):
+    for i in range(num):
+        username = f"user{i}"
+        new_user = User(username=username)
+        db.add(new_user)
+
+    db.commit()
+    db.refresh(new_user)
